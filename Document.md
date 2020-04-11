dafb6a293401d5a31b750c106e19aacc650b1069
Junho Baik <junhobaik@gmail.com>
2020-04-11 23:37:46 +0900

    redux 설치 및 적용

--- Document.md
+++ Document.md
@@ -1,9 +1,11 @@
 # Development Document
 
 - npm, git init
-
 - webpack, react 기반의 HMR이 가능한 보일러플레이트를 가져옴
-
 - 가져온 보일러 플레이트를 수정
   - 폴더 구조 변경
   - sass 추가
+- 타입스크립트 적용
+  - import image 관련 오류 발생(모듈을 찾을 수 없습니다) d.ts 추가  
+    (https://stackoverflow.com/questions/36148639/webpack-not-able-to-import-images-using-express-and-angular2-in-typescript/36151803#36151803)
+- 차후 리덕스 사용을 위한 리덕스 스토어 및 루트 리듀서 작성
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../modules";
+
 import App from "../components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
