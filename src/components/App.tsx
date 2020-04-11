721e3de2219ccbcd6ff72f91e27c82d2c950cd91
Junho Baik <junhobaik@gmail.com>
2020-04-12 01:18:55 +0900

    header, feeds, tabs 컴포넌트 마크업 포지션 기본 구현

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,25 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
+import "./app.scss";
+import Tabs from "./Tabs";
+import Feeds from "./Feeds";
+import Header from "./Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div id="App">
+      <Header />
+
+      <div className="app-bottom">
+        <div className="app-bottom-left">
+          <Tabs />
+        </div>
+        <div className="app-bottom-right">
+          <Feeds />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default hot(module)(App);
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.scss";
+
+const Header = () => {
+  return <div id="Header">header</div>;
+};
+
+export default Header;
