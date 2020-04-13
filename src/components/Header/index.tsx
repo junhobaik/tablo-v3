aa10f6424de97b3fd6940c0bcb9035afe5b35589
Junho Baik <junhobaik@gmail.com>
2020-04-13 23:36:18 +0900

    스타일 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,12 @@ import React from "react";
 import "./index.scss";
 
 const Header = () => {
-  return <div id="Header"></div>;
+  return (
+    <div id="Header">
+      <h1>Tablo</h1>
+      <button>Global Setting</button>
+    </div>
+  );
 };
 
 export default Header;
