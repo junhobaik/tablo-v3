b6645dae51e5b664f68b5a4e44301a2c9d72b3f3
Junho Baik <junhobaik@gmail.com>
2020-06-06 01:31:43 +0900

    Popup, 구현 사항
    - 현재 방문중인 페이지(개발을 위해 현재 활성중인 윈도우의 첫번째 탭으로 설정)의 정보를 불러오고 추가 할 수 있도록 구현 및 마크업
    - 현재 방문장인 사이트가 피드 추가가 가능한지 검사하고 정보를 불러와 추가 할 수 있도록 구현 및 마크업
    - 추가할 콜렉션 선택 및 추가 관련 사항은 미구현

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,13 +1,219 @@
+/* eslint @typescript-eslint/no-unused-vars: 0 */
+/* eslint no-fallthrough: 0 */
+/* eslint no-sparse-arrays: 0 */
+/* eslint no-unused-vars: 0 */
+
 import * as React from "react";
-import icon from "../../img/icon-128.png";
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon as Fa } from "@fortawesome/react-fontawesome";
+import { faFile } from "@fortawesome/free-regular-svg-icons";
+
 import { hot } from "react-hot-loader";
 import "./index.scss";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { RootState } from "../../modules";
+import _ from "lodash";
+
+interface Site {
+  title: string;
+  url: string;
+  favIconUrl: string | undefined;
+}
+
+interface Feed {
+  title: string;
+  siteUrl: string;
+  feedUrl: string;
+}
 
 const Popup = () => {
+  const [site, setSite] = useState<Site>();
+  const [feed, setFeed] = useState<Feed>();
+  const [isFeedSearchDone, setIsFeedSearchDone] = useState(false);
+  const [siteTitle, setSiteTitle] = useState<string>("");
+  const [feedTitle, setFeedTitle] = useState<string>("");
+  const [state, setState] = useState<RootState>();
+  const [firstLoadDone, setFirstLoadDone] = useState<boolean>(false);
+  const [containFeeds, setContainFeeds] = useState<boolean>(false);
+
+  const delay = (s: number = 2000) => {
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, s)
+    );
+  };
+
+  const feedCheck = async (siteUrl: string) => {
+    const url = siteUrl.split("/").splice(0, 3).join("/");
+    const urlCheck = ["feed", "rss", "feed.xml", "rss.xml", "d2.atom"];
+
+    const request = async (c: string) => {
+      try {
+        const requestUrl = `https://api.rss2json.com/v1/api.json?rss_url=${url}/${c}`;
+        const response = await fetch(requestUrl);
+
+        if (response.ok) {
+          const feedData = await response.json();
+          const { title } = feedData.feed;
+
+          setFeed({
+            title: title ?? "Untitled",
+            siteUrl: url,
+            feedUrl: `${url}/${c}`,
+          });
+          setFeedTitle(title ?? "Untitled");
+
+          return response.status;
+        } else {
+          throw response.status;
+        }
+      } catch (status) {
+        if (typeof status !== "number") {
+          await delay();
+          await request(c);
+        }
+      }
+    };
+
+    for (const c of urlCheck) {
+      await request(c);
+      await delay();
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    if (firstLoadDone) {
+      // chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      chrome.tabs.query({ currentWindow: false, active: true }, function (
+        tabs
+      ) {
+        const { title, url, favIconUrl } = tabs[0];
+
+        setSite({
+          title: title ?? "Untitled",
+          url: url ?? "",
+          favIconUrl,
+        });
+        setSiteTitle(title ?? "Untitled");
+        const findedFeeds = _.find(state?.feeds.feeds, ["siteUrl", url?.split("/").splice(0, 3).join("/")]);
+        console.log(findedFeeds);
+        setContainFeeds(findedFeeds ? true : false);
+
+        if (url && !findedFeeds) {
+          feedCheck(url).then(() => {
+            setIsFeedSearchDone(true);
+          });
+        }
+      });
+    } else {
+      chrome.storage.sync.get("tablo3", (res) => {
+        if (res.tablo3) {
+          setState(res.tablo3);
+          setFirstLoadDone(true);
+        }
+      });
+    }
+  }, [firstLoadDone]);
+
   return (
-    <div id="popup">
-      <h1>popup</h1>
-      <img src={icon} />
+    <div id="Popup">
+      <h1>Tablo</h1>
+      {site && site.url.length ? (
+        <>
+          <h2>Add Link to Tabs</h2>
+          <div className="site item-wrap">
+            <div className="favicon-wrap">
+              <div className="no-favicon">
+                <Fa icon={faFile} />
+              </div>
+              <img
+                className="favicon"
+                src={site.favIconUrl ?? ""}
+                onError={(e: any) => {
+                  e.currentTarget.style.display = "none";
+                  e.currentTarget.parentNode.firstChild.style.display = "flex";
+                }}
+              />
+            </div>
+
+            <input
+              type="text"
+              className="title-input"
+              value={siteTitle}
+              placeholder={site.title}
+              onChange={(e) => {
+                setSiteTitle(e.currentTarget.value);
+              }}
+            />
+
+            <div className="add-submit submit">
+              <select>
+                <option value="cart">Cart</option>
+              </select>
+              <button>
+                <Fa icon={faPlus} />
+              </button>
+            </div>
+          </div>
+
+          <h2>Add Feed to Feeds</h2>
+          {isFeedSearchDone ? (
+            <div className="add-feed">
+              {feed ? (
+                <div className="feed item-wrap">
+                  <div className="favicon-wrap">
+                    <div className="no-favicon">
+                      <Fa icon={faFile} />
+                    </div>
+                    <img
+                      className="favicon"
+                      src={`${feed.siteUrl}/favicon.ico` ?? ""}
+                      onError={(e: any) => {
+                        e.currentTarget.style.display = "none";
+                        e.currentTarget.parentNode.firstChild.style.display =
+                          "flex";
+                      }}
+                    />
+                  </div>
+
+                  <input
+                    type="text"
+                    className="title-input"
+                    value={feedTitle}
+                    placeholder={feed.title}
+                    onChange={(e) => {
+                      setFeedTitle(e.currentTarget.value);
+                    }}
+                  />
+                  <div className="submit">
+                    <select>
+                      <option value="collection">collection</option>
+                    </select>
+                    <button>
+                      <Fa icon={faPlus} />
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div className="no-feed">
+                  <span>피드 주소를 확인 할 수 없습니다.</span>
+                </div>
+              )}
+            </div>
+          ) : containFeeds ? (
+            <div className="contain-feed">
+              <span>이미 존재하는 FEED.</span>
+            </div>
+          ) : (
+            <div className="search-feed">
+              <span>FEED 주소 검색 중...</span>
+            </div>
+          )}
+        </>
+      ) : null}
     </div>
   );
 };
