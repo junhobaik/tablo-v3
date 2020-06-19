6c15f1b8288da30a98fc527c1247f1013e44ec8e
Junho Baik <junhobaik@gmail.com>
2020-06-20 04:06:26 +0900

    webpack 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,71 @@
 /* eslint-disable */
-const webpack = require("webpack");
-const path = require("path");
-const fileSystem = require("fs");
-const env = require("./utils/env");
-const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WriteFilePlugin = require("write-file-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const fileSystem = require('fs');
+const env = require('./utils/env');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const secretsPath = path.join(__dirname, "secrets." + env.NODE_ENV + ".js");
+const secretsPath = path.join(__dirname, 'secrets.' + env.NODE_ENV + '.js');
 
 const alias = {
-  "react-dom": "@hot-loader/react-dom",
+  'react-dom': '@hot-loader/react-dom',
 };
 
-const fileExtensions = [
-  "jpg",
-  "jpeg",
-  "png",
-  "gif",
-  "eot",
-  "otf",
-  "svg",
-  "ttf",
-  "woff",
-  "woff2",
-];
+const fileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'eot', 'otf', 'svg', 'ttf', 'woff', 'woff2'];
 
 if (fileSystem.existsSync(secretsPath)) {
-  alias["secrets"] = secretsPath;
+  alias['secrets'] = secretsPath;
 }
 
 const options = {
-  mode: process.env.NODE_ENV || "development",
+  mode: process.env.NODE_ENV || 'development',
   entry: {
-    index: path.join(__dirname, "src", "pages", "index.tsx"),
-    popup: path.join(__dirname, "src", "pages", "popup.tsx"),
-    options: path.join(__dirname, "src", "pages", "options.tsx"),
-    background: path.join(__dirname, "src", "pages", "background.ts"),
+    index: path.join(__dirname, 'src', 'pages', 'index.tsx'),
+    popup: path.join(__dirname, 'src', 'pages', 'popup.tsx'),
+    options: path.join(__dirname, 'src', 'pages', 'options.tsx'),
+    background: path.join(__dirname, 'src', 'pages', 'background.ts'),
   },
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js",
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
-        options: {
-          // eslint options (if necessary)
-        },
+        loader: 'eslint-loader',
+        options: {},
       },
       {
         test: /\.scss/,
         use: [
-          process.env.NODE_ENV !== "production"
-            ? "style-loader"
-            : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
-        test: new RegExp(".(" + fileExtensions.join("|") + ")$"),
-        loader: "file-loader?name=[name].[ext]",
+        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+        loader: 'file-loader?name=[name].[ext]',
         exclude: /node_modules/,
       },
       {
         test: /\.html$/,
-        loader: "html-loader",
+        loader: 'html-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.(js|jsx)$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
       },
       {
@@ -88,7 +73,7 @@ const options = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
           },
         ],
       },
@@ -97,16 +82,16 @@ const options = {
   resolve: {
     alias: alias,
     extensions: fileExtensions
-      .map((extension) => "." + extension)
-      .concat([".ts", ".tsx", ".jsx", ".js", ".css", ".scss"]),
+      .map((extension) => '.' + extension)
+      .concat(['.ts', '.tsx', '.jsx', '.js', '.css', '.scss']),
   },
   plugins: [
     new MiniCssExtractPlugin(),
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
     // new CleanWebpackPlugin(),
-    new webpack.EnvironmentPlugin(["NODE_ENV"]),
     new CopyWebpackPlugin([
       {
-        from: "src/manifest.json",
+        from: 'src/manifest.json',
         transform: function (content, path) {
           return Buffer.from(
             JSON.stringify({
@@ -119,32 +104,32 @@ const options = {
       },
     ]),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "pages", "index.html"),
-      filename: "index.html",
-      chunks: ["index"],
+      template: path.join(__dirname, 'src', 'pages', 'index.html'),
+      filename: 'index.html',
+      chunks: ['index'],
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "pages", "popup.html"),
-      filename: "popup.html",
-      chunks: ["popup"],
+      template: path.join(__dirname, 'src', 'pages', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup'],
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "pages", "options.html"),
-      filename: "options.html",
-      chunks: ["options"],
+      template: path.join(__dirname, 'src', 'pages', 'options.html'),
+      filename: 'options.html',
+      chunks: ['options'],
     }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "pages", "background.html"),
-      filename: "background.html",
-      chunks: ["background"],
+      template: path.join(__dirname, 'src', 'pages', 'background.html'),
+      filename: 'background.html',
+      chunks: ['background'],
     }),
     new WriteFilePlugin(),
   ],
 };
 
-if (env.NODE_ENV === "development") {
+if (env.NODE_ENV === 'development') {
   // options.devtool = "cheap-module-eval-source-map";
-  options.devtool = "source-map";
+  options.devtool = 'source-map';
 }
 
 module.exports = options;
