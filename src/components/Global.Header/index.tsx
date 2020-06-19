4114ea4716bf3f72ff208ce6eb9575af13789503
Junho Baik <junhobaik@gmail.com>
2020-06-19 23:23:45 +0900

    Global.Header, title 옆에 version 표시

--- src/components/Global.Header/index.tsx
+++ src/components/Global.Header/index.tsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { FontAwesomeIcon as Fa } from "@fortawesome/react-fontawesome";
-import { faCog } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import { FontAwesomeIcon as Fa } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
-import "./index.scss";
+import './index.scss';
+import manifest from '../../manifest.json';
 
 const Header = (props: { setIsSettingModal: Function }) => {
   return (
     <div id="Header">
-      <h1>Tablo</h1>
+      <div className="title-wrap">
+        <h1>Tablo</h1>
+        <span>v{manifest.version}</span>
+      </div>
       <button
         onClick={() => {
           props.setIsSettingModal(true);
